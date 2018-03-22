7e5de7500f64de7795b55defd445917d11c020f0
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-03-22 10:12:43 +0100

    SubPageServlet created, from now on this servlet will handle, which kind of subpage and which subpage exactly to send to the proper jsp file.

---
+++ src/main/java/com/codecool/web/servlet/SubPageServlet.java
@@ -0,0 +1,37 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.AssignmentPage;
+import com.codecool.web.model.SubPage;
+import com.codecool.web.model.TextPage;
+import com.codecool.web.service.DataStorage;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/sub-page")
+public class SubPageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int subPageId = Integer.parseInt(req.getParameter("id"));
+        SubPage tempSubPage = DataStorage.getInstance().getAllSubPages().get(subPageId);
+        String dispatcher = "";
+
+        if (tempSubPage instanceof AssignmentPage){
+            AssignmentPage tempAssign = (AssignmentPage) tempSubPage;
+            req.setAttribute("ap",tempAssign);
+            dispatcher = "displayAssignPage.jsp";
+
+        } else if (tempSubPage instanceof TextPage){
+            TextPage tempText = (TextPage) tempSubPage;
+            req.setAttribute("tp",tempText);
+            dispatcher = "displayTextPage.jsp";
+        }
+
+        req.getRequestDispatcher(dispatcher).forward(req, resp);
+    }
+}
