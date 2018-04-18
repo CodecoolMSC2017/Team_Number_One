a392d3e4409d83b602048e539b2dff604eaf63bf
hreskonorbert <norberthresko@gmail.com>
2018-04-18 11:20:39 +0200

    Modified SubPageServlet and TextPageServlet to use Services instead of DataStorage

--- src/main/java/com/codecool/web/servlet/SubPageServlet.java
+++ src/main/java/com/codecool/web/servlet/SubPageServlet.java
@@ -1,9 +1,11 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.SubPageDao;
 import com.codecool.web.model.AssignmentPage;
 import com.codecool.web.model.SubPage;
 import com.codecool.web.model.TextPage;
 import com.codecool.web.service.DataStorage;
+import com.codecool.web.service.SubPageService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,27 +13,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/sub-page")
 public class SubPageServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int subPageId = Integer.parseInt(req.getParameter("id"));
-        SubPage tempSubPage = DataStorage.getInstance().getAllSubPages().get(subPageId);
-        String dispatcher = "";
+        try (Connection connection = getConnection(req.getServletContext())) {
+            int subPageId = Integer.parseInt(req.getParameter("id"));
+            SubPageDao spDao = new SubPageDao(connection);
+            SubPageService spService = new SubPageService(spDao);
+            SubPage tempSubPage = spService.getAllSubPages().get(subPageId);
+            String dispatcher = "";
 
-        if (tempSubPage instanceof AssignmentPage){
-            AssignmentPage tempAssign = (AssignmentPage) tempSubPage;
-            req.setAttribute("ap",tempAssign);
-            dispatcher = "protected/displayAssignPage.jsp";
+            if (tempSubPage instanceof AssignmentPage) {
+                AssignmentPage tempAssign = (AssignmentPage) tempSubPage;
+                req.setAttribute("ap", tempAssign);
+                dispatcher = "protected/displayAssignPage.jsp";
 
-        } else if (tempSubPage instanceof TextPage){
-            TextPage tempText = (TextPage) tempSubPage;
-            req.setAttribute("tp",tempText);
-            dispatcher = "protected/displayTextPage.jsp";
+            } else if (tempSubPage instanceof TextPage) {
+                TextPage tempText = (TextPage) tempSubPage;
+                req.setAttribute("tp", tempText);
+                dispatcher = "protected/displayTextPage.jsp";
+            }
+
+            req.getRequestDispatcher(dispatcher).forward(req, resp);
+        }catch (SQLException ex){
+            ex.printStackTrace();
         }
-
-        req.getRequestDispatcher(dispatcher).forward(req, resp);
     }
 }
