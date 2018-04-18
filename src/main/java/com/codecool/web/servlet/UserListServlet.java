bfacfac5c9c8971470e9848afa7c5cc4ffd8f6d7
hreskonorbert <norberthresko@gmail.com>
2018-04-18 10:41:14 +0200

    Modofied UserListServlet to use service instead of DataStorage

--- src/main/java/com/codecool/web/servlet/UserListServlet.java
+++ src/main/java/com/codecool/web/servlet/UserListServlet.java
@@ -1,7 +1,10 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.DatabaseUserDao;
+import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.User;
 import com.codecool.web.service.DataStorage;
+import com.codecool.web.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,22 +12,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet("/userList")
 public class UserListServlet extends AbstractServlet {
-    List<User> users = DataStorage.getInstance().getUserList();
+
 
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getParameter("id") != null) {
-            for (User user : users) {
-                if (user.getUniqueId().equals(req.getParameter("id"))) {
-                    req.setAttribute("profile", user);
-                    req.getRequestDispatcher("protected/userProfile.jsp").forward(req, resp);
+        try(Connection connection = getConnection(req.getServletContext())) {
+            UserDao userDao = new DatabaseUserDao(connection);
+            UserService userService = new UserService(userDao);
+            List<User> users = userService.getUserList();
+            if (req.getParameter("id") != null) {
+                for (User user : users) {
+                    if (Integer.toString(user.getUniqueId()).equals(req.getParameter("id"))) {
+                        req.setAttribute("profile", user);
+                        req.getRequestDispatcher("protected/userProfile.jsp").forward(req, resp);
+                    }
                 }
             }
+        }catch (SQLException ex){
+            ex.printStackTrace();
         }
     }
 }
