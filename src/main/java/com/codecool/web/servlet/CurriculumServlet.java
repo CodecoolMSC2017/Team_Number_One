ce6bb1949c06fe93ce1b248f1529f7a9fa9eaf2a
Laszlo Poroszkai <laszlo.poroszkai@gmail.com>
2018-04-19 13:30:20 +0200

    cleanup

--- src/main/java/com/codecool/web/servlet/CurriculumServlet.java
+++ src/main/java/com/codecool/web/servlet/CurriculumServlet.java
@@ -9,19 +9,13 @@ import com.codecool.web.model.TextPage;
 import com.codecool.web.model.User;
 import com.codecool.web.service.*;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/curriculum")
--- src/main/java/com/codecool/web/servlet/ProfileUpdateServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileUpdateServlet.java
@@ -11,7 +11,6 @@ import com.codecool.web.service.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
